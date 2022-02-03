60e12f82763a20639509810a2f80c65937724c10
songyunhwa <46363234+songyunhwa@users.noreply.github.com>
2022-02-03 13:30:52 +0900

    Delete mysql.js

--- mysql.js
+++
@@ -1,8 +0,0 @@
-var mysql      = require('mysql');
-var connection ={
-  host     : 'localhost',    // 호스트 주소
-  user     : 'root',           // mysql user
-  password : 'password1234!',       // mysql password
-  database : 'node_app'         // mysql 데이터베이스
-};
-module.exports = mysql.createPool(connection);
